e6b4ef9db848f31065ad293c915d1d77585617c6
BestBearEver <61414449+BestBearEver@users.noreply.github.com>
2021-02-06 20:07:51 +0000

    At Last
    Hateful Github -18th try!

---
+++ Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@E:\daisyImmisivetemplate-pc\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1666643873826340960.json
